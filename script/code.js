917be159e6ea25a4e948ecb4d273fc45eb338b40
Mzwa1080 <mshawnlouw@gmail.com>
2023-10-10 12:24:16 +0200

    pushin basics naw

--- script/code.js
+++ script/code.js
@@ -1,1 +1,9 @@
-console.log('Mzwamadoda Shawn Louw')
+let numb1 = '3'
+let numb2 =3
+
+console.log(`sum of ${numb1} and ${numb2} is $${numb1+numb2}`)
+console.log(`Difference of ${numb2} and ${numb1} is $${numb2+numb1}`)
+console.log(`Product of ${numb1} and ${numb2} is $${numb1*numb2}`)
+console.log(`${numb2} /${numb1} is $${numb2/numb1}`)
+
+console.log(numb1 == numb2)
